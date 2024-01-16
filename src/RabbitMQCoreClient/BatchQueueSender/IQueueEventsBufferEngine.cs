9eedeb5a51ac31010bff220371742a641bb13ce6
2nnar <lobovyroslav@gmail.com>
2024-01-16 11:38:51 +0300

    🔖 6.0.5, return AddEvent method overloading for multiple events as obsolete for backward compatibility

--- src/RabbitMQCoreClient/BatchQueueSender/IQueueEventsBufferEngine.cs
+++ src/RabbitMQCoreClient/BatchQueueSender/IQueueEventsBufferEngine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace RabbitMQCoreClient.BatchQueueSender
@@ -15,6 +16,16 @@ namespace RabbitMQCoreClient.BatchQueueSender
         /// <param name="routingKey">The name of the route key with which you want to send events to the data bus.</param>
         /// <returns><see cref="Task"/> showing the completion of the operation.</returns>
         Task AddEvent<T>(T @event, string routingKey);
+
+        /// <summary>
+        /// Add events to send to the data bus.
+        /// </summary>
+        /// <typeparam name="T">The type of list item of the <paramref name="events"/> property.</typeparam>
+        /// <param name="events">The list of objects to send to the data bus.</param>
+        /// <param name="routingKey">The name of the route key with which you want to send events to the data bus.</param>
+        /// <returns></returns>
+        [Obsolete("Use AddEvents instead.")]
+        Task AddEvent<T>(IEnumerable<T> events, string routingKey);
 
         /// <summary>
         /// Add events to send to the data bus.
